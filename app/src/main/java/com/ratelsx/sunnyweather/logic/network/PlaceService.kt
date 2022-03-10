6eba0b2eb1aec7b2550a713006aee6f9a8139d1d
Ratel <350790090@qq.com>
2022-03-10 20:56:28 +0800

    添加城市数据网络层接口

---
+++ app/src/main/java/com/ratelsx/sunnyweather/logic/network/PlaceService.kt
@@ -0,0 +1,19 @@
+package com.ratelsx.sunnyweather.logic.network
+
+import com.ratelsx.sunnyweather.SunnyWeatherApplication
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+/**
+ * 城市数据网络层接口
+ */
+interface PlaceService {
+
+    /**
+     * 城市数据接口
+     * @param query 需要查询的城市
+     */
+    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
+    fun searchPlaces(@Query("query") query: String): Call<PlaceService>
+}
