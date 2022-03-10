11d88a66dc8c91b9016c9ab710abb068a932e38d
Ratel <350790090@qq.com>
2022-03-10 22:10:14 +0800

    删除错误导包

--- app/src/main/java/com/ratelsx/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/ratelsx/sunnyweather/logic/network/PlaceService.kt
@@ -1,6 +1,7 @@
 package com.ratelsx.sunnyweather.logic.network
 
 import com.ratelsx.sunnyweather.SunnyWeatherApplication
+import com.ratelsx.sunnyweather.logic.model.PlaceResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -15,5 +16,5 @@ interface PlaceService {
      * @param query 需要查询的城市
      */
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
-    fun searchPlaces(@Query("query") query: String): Call<PlaceService>
+    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 }
